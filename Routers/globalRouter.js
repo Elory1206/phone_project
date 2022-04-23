051e14c8c96c9430e267990d2988d52caba82a9a
정근모 <jeong-geunmo@jeong-geunmoui-MacBookAir.local>
2022-04-23 15:32:57 +0900

    a

--- Routers/globalRouter.js
+++ Routers/globalRouter.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const glboalControllers = require("../Controllers/globalController");
 
 const router = express.Router();
 
-router.get("/", glboalControllers.indexController);
-
-router.get("/intro", glboalControllers.introController);
-
-router.get("/product", glboalControllers.productController);
+router.get("/", (req, res, next) => {
+    res.render("screens/home");
+});
 
 module.exports = router;
